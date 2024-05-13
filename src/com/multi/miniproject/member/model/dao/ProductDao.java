5838b8e319c4e192cb5a30faebff4dc3eb0f1dd4
Kang Hyungoo <gooiinn@outlook.kr>
2024-05-13 15:06:12 +0900

    ProductDao 주요 메소드 구현(PRODUCTS CRUD 완료)

--- src/com/multi/miniproject/member/model/dao/ProductDao.java
+++ src/com/multi/miniproject/member/model/dao/ProductDao.java
@@ -9,6 +9,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class ProductDao {
 
@@ -54,5 +55,131 @@ public class ProductDao {
         return rsDto;
     } //getCarDto() : ProductDto를 파라미터로 넣으면 이것에 대응되는 CarDto를 리턴해주는 함수
 
+    public int insert(ProductDto productDto) {
+        int result = 0;
+        PreparedStatement ps = null;
+
+        try {
+            String sql = "INSERT INTO PRODUCTS VALUES('P'||PRODUCT_NUM_SEQ.NEXTVAL, ?, ?, ?, ?)";
+            ps = con.prepareStatement(sql);
+            ps.setString(1, productDto.getCarNum());
+            ps.setString(2, productDto.getProductStatus());
+            ps.setInt(3, productDto.getProductPrice());
+            ps.setInt(4, productDto.getProductAvailable());
+
+            result = ps.executeUpdate();
+            if(result > 0) con.commit();
+            else con.rollback();
+
+        } catch(SQLException e) {
+            e.printStackTrace();
+            System.out.println("insert(Product)시 에러발생");
+
+        } finally {
+            dbcp.freeConnection(con, ps);
+        }
+
+        return result;
+    } //insert()
+
+    public int delete(String productNum) {
+        int result = 0;
+        PreparedStatement ps = null;
+
+        try {
+            String sql = "DELETE FROM PRODUCTS WHERE PRODUCT_NUM = ?";
+            ps = con.prepareStatement(sql);
+            ps.setString(1, productNum);
+
+            result = ps.executeUpdate();
+            if(result > 0) con.commit();
+            else con.rollback();
+
+        } catch(SQLException e) {
+            e.printStackTrace();
+            System.out.println("delete(Product)시 에러발생");
+
+        } finally {
+            dbcp.freeConnection(con, ps);
+        }
+
+        return result;
+    } //delete()
+
+    public ProductDto selectOne(String productNum) {
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        ProductDto rsDto = null;
+
+        try {
+            String sql = "SELECT * FROM PRODUCTS WHERE PRODUCT_NUM = ?";
+            ps = con.prepareStatement(sql);
+            ps.setString(1, productNum);
+            rs = ps.executeQuery();
+
+            if(rs.next()){
+                rsDto = new ProductDto();
+                rsDto.setProductNum(rs.getString("PRODUCT_NUM"));
+                rsDto.setCarNum(rs.getString("CAR_NUM"));
+                rsDto.setProductStatus(rs.getString("PRODUCT_STATUS"));
+                rsDto.setProductPrice(rs.getInt("PRODUCT_PRICE"));
+                rsDto.setProductAvailable(rs.getInt("PRODUCT_AVAILABLE"));
+            }
+        } catch(SQLException e) {
+            System.out.println("selectOne(productNum)시 에러발생");
+        } finally {
+            dbcp.freeConnection(con, ps, rs);
+        }
+
+        return rsDto;
+
+    } //selectOne()
+
+    public ArrayList<ProductDto> selectList(int criteria, String keyword) {
+        ArrayList<ProductDto> rsDtoList = new ArrayList<>();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+            String sql = "";
+            switch(criteria){ //검색조건
+                case 0:
+                    sql += "SELECT * FROM PRODUCTS WHERE PRODUCT_NUM = ?";
+                    break;
+                case 1:
+                    sql += "SELECT * FROM PRODUCTS WHERE CAR_NUM = ?";
+                    break;
+                case 2:
+                    sql += "SELECT * FROM PRODUCTS WHERE PRODUCT_STATUS = ?";
+                    break;
+                case 3:
+                    sql += "SELECT * FROM PRODUCTS WHERE PRODUCT_PRICE = ?";
+                    break;
+                case 4:
+                    sql += "SELECT * FROM PRODUCTS WHERE PRODUCT_AVAILABLE = ?";
+                    break;
+            }
+            ps = con.prepareStatement(sql);
+            ps.setString(1, keyword); //검색어
+            rs = ps.executeQuery();
+
+            while(rs.next()) {
+                ProductDto productDto = new ProductDto();
+                productDto.setProductNum(rs.getString("PRODUCT_NUM"));
+                productDto.setCarNum(rs.getString("CAR_NUM"));
+                productDto.setProductStatus(rs.getString("PRODUCT_STATUS"));
+                productDto.setProductPrice(rs.getInt("PRODUCT_PRICE"));
+                productDto.setProductAvailable(rs.getInt("PRODUCT_AVAILABLE"));
+
+                rsDtoList.add(productDto);
+            }
+        } catch(SQLException e) {
+            System.out.println("selectList(Product)시 에러발생");
+        } finally {
+            dbcp.freeConnection(con, ps, rs);
+        }
+
+        return rsDtoList;
+    } //selectList()
 
 }
