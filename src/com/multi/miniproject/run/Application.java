23e8e4fc034ab8d9ac5f37c3b0aa2ef11039718b
JinSuKyeong <awerty4565@gmail.com>
2024-05-13 19:42:09 +0900

    mypage

--- src/com/multi/miniproject/run/Application.java
+++ src/com/multi/miniproject/run/Application.java
@@ -4,6 +4,7 @@ import com.multi.miniproject.common.view.UI;
 
 public class Application {
     public static void main(String[] args) {
+//        new UI().p03B();
         new UI().p01();
     }
 }
