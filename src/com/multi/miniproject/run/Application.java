76db09c7b28a5a44de1561382b51e71843ab783e
Kang Hyungoo <gooiinn@outlook.kr>
2024-05-14 10:06:42 +0900

    컨플릭트 정리

--- src/com/multi/miniproject/run/Application.java
+++ src/com/multi/miniproject/run/Application.java
@@ -4,7 +4,7 @@ import com.multi.miniproject.common.view.UI;
 
 public class Application {
     public static void main(String[] args) {
-//        new UI().p03B();
-        new UI().p01();
+        new UI().p03B_3();
+//        new UI().p01();
     }
 }
